bf9bb8a390e6fa0b3c2ed8cb077e38a5ec911227
S331471193 <S331471193@LDHS-BCT-100921.ad.ocdsb.ca>
2019-10-22 09:18:04 -0400

    add new stuff

---
+++ src/shapes/Circular.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shapes;
+
+/**
+ *
+ * @author S331471193
+ */
+public class Circular extends Shapes{
+    double radius;
+    double circ;
+    
+    double getRad(){
+        return radius;
+    }
+    double getCirc(){
+        return circ;
+    }
+}
